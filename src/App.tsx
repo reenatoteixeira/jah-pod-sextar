ccc990bc2ad51882a92be10d3141ad5f108c3134
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-12 12:56:43 -0300

    feat: refactor App.tsx for improved layout and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import viteLogo from './assets/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main
+        className="h-screen flex flex-col gap-8 justify-center items-center container max-w-7xl mx-auto text-center">
+        <div className="flex items-center justify-center w-full">
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo}
+                 className="h-40 p-6 transition duration-300 will-change-[filter] hover:drop-shadow-[0_0_2em_#646cffaa]"
+                 alt="Vite logo"/>
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo}
+                 className="h-40 p-6 transition duration-300 will-change-[filter] animate-[spin_20s_linear_infinite] hover:drop-shadow-[0_0_2em_#61dafbaa]"
+                 alt="React logo"/>
+          </a>
+        </div>
+        <h1 className="text-5xl font-bold">JahPodSextar is migrating!</h1>
+
+        <div className="p-12 text-lg">
+          <p>Soon, this will be a React + Vite / Typescript app!</p>
+        </div>
+      </main>
     </>
   )
 }

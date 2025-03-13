6d935e54e8cb25118f8281f98a7fe34e91f01bbf
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-13 14:35:14 -0300

    feat: add Answer component for rendering answer text

---
+++ src/components/Answer.tsx
@@ -0,0 +1,12 @@
+interface AnswerProps {
+  answer: string;
+}
+
+function Answer({answer}: AnswerProps) {
+  return (
+    <p className={'text-lg sm:text-2xl md:text-3xl'}>
+      {answer}</p>
+  )
+}
+
+export default Answer;

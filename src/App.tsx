4a6961abddbbade6e08071cb56f115a09e2a662e
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-13 23:30:26 -0300

    feat: implement dynamic response retrieval in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import Title from "./components/Title.tsx";
 import Answer from "./components/Answer.tsx";
 import Footer from "./components/Footer.tsx";
+import {useState, useEffect} from "react";
+import {getResponse} from "./utils/getResponse.ts";
 
 function App() {
+  const [response, setResponse] = useState('Pensando...');
+
+  useEffect(() => {
+    setResponse(getResponse());
+  }, [])
 
   return (
     <>
@@ -10,7 +17,7 @@ function App() {
         <div
           className={'w-full flex flex-col justify-center items-center gap-4 text-center container max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'}>
           <Title title={'Jah pod sextar?'}/>
-          <Answer answer={'Lorem ipsum dolor sit amet'}/>
+          <Answer answer={response}/>
         </div>
       </main>
 

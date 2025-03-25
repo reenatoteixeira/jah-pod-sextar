a22ecb71c8cf473d0bbfbb221d588237a3fd9848
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-24 22:51:14 -0300

    feat: add ThemeSelect component for theme selection functionality

---
+++ src/components/ThemeSelect.tsx
@@ -0,0 +1,25 @@
+import availableThemes from '../themes/availableThemes.ts'
+
+interface ThemeSelectProps {
+  onChange: (theme: string) => void;
+}
+
+function ThemeSelect({onChange}: ThemeSelectProps) {
+  const themes = availableThemes;
+
+  return (
+    <select name="themes" id="theme-select" onChange={(e) => onChange(e.target.value)} defaultValue="default"
+            title="Select theme" className='appearance-auto pr-2 cursor-pointer'>
+      <option value="default" disabled hidden>Select theme</option>
+      <option value="system" className='text-slate-950'>System</option>
+      {themes.map(theme => (
+        <option key={`theme--${theme.toString().toLowerCase().split(' ').join('-')}`}
+                value={`theme--${theme.toString().toLowerCase().split(' ').join('-')}`} className='text-slate-950'>
+          {theme}
+        </option>
+      ))}
+    </select>
+  )
+}
+
+export default ThemeSelect;

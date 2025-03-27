2a6dca8c0b27840b12cf85173e71df7ab47ac276
Renato Teixeira <renato.gfteixeira@outlook.com>
2025-03-27 16:10:47 -0300

    feat: integrate i18n support in getResponse function for localized responses

--- src/utils/getResponse.ts
+++ src/utils/getResponse.ts
@@ -1,9 +1,13 @@
-import responsesJSON from '../data/responses.json'
 import checkWeekDayAndHour from './checkWeekDayAndHour.ts'
+import { TFunction } from 'i18next'
+import i18n from '../i18n.ts'
 
-export function getResponse() {
-  const day = checkWeekDayAndHour() as keyof typeof responsesJSON,
-    key = Math.floor(Math.random() * responsesJSON[day].length)
+export function getResponse(t: TFunction) {
+  console.log('i18n:', Object.keys(i18n.services.resourceStore.data))
 
-  return responsesJSON[day][key]
+  const day = checkWeekDayAndHour(),
+    responses = t(`responses.${day}`, { returnObjects: true }) as string[],
+    key = Math.floor(Math.random() * responses.length)
+
+  return responses[key]
 }
